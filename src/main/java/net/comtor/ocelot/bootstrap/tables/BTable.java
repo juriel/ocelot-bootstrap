18c1f06fd4b1e73abd0e4e5757b437629134ec78
Jaime Uriel Torres Serrano <juriel@comtor.net>
2019-10-25 20:49:23 -0500

    remove methods From BTable.

--- src/main/java/net/comtor/ocelot/bootstrap/tables/BTable.java
+++ src/main/java/net/comtor/ocelot/bootstrap/tables/BTable.java
@@ -14,36 +14,6 @@ import net.comtor.ocelot.html.tables.HtmlTr;
  */
 public class BTable extends HtmlTable {
 
-    public BTable addHeaders(String... headers) {
-        addHeaders(Arrays.asList(headers));
-        return this;
-    }
 
-    public BTable addHeaders(List<String> headers) {
-        HtmlTr tr = new HtmlTr();
-        headers.forEach((header) -> {
-            tr.add(new HtmlTh(header));
-        });
-        addFirst(tr);
-        return this;
-    }
-
-    public BTable addRow(String... celds) {
-        HtmlTr tr = new HtmlTr();
-        Arrays.asList(celds).forEach((celd) -> {
-            tr.add(new HtmlTd(celd));
-        });
-        add(tr);
-        return this;
-    }
-
-    public BTable addRow(HtmlTag... tags) {
-        HtmlTr tr = new HtmlTr();
-        Arrays.asList(tags).forEach((tag) -> {
-            tr.add(new HtmlTd(tag));
-        });
-        add(tr);
-        return this;
-    }
 
 }
