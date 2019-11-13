837c830373dbad3189ea74a3e2d110073bab3b32
Juan Diego Espinosa Santos <juandiego@comtor.net>
2019-11-13 10:23:04 -0500

    * BShowField

--- src/main/java/net/comtor/ocelot/bootstrap/forms/BShowField.java
+++ src/main/java/net/comtor/ocelot/bootstrap/forms/BShowField.java
@@ -1,64 +1,57 @@
-//package net.comtor.ocelot.bootstrap.forms;
-//
-//import net.comtor.ocelot.bootstrap.decorators.BootstrapFormElement;
-//import net.comtor.ocelot.html.forms.HtmlFieldset;
-//import net.comtor.ocelot.html.forms.HtmlFormElement;
-//import net.comtor.ocelot.html.forms.inputs.HtmlInputHidden;
-//import org.apache.commons.lang3.StringUtils;
-//
-///**
-// *
-// * @author Guido Cafiel
-// */
-//public class BShowField extends BootstrapFormElement {
-//
-//    HtmlInputHidden htmlHidden;
-//    HtmlFieldset fieldset;
-//
-//    public BShowField(String name, String label, String hidden, String visible) {
-//        super(name + "_visible", label, new HtmlFieldset());
-//        fieldset = (HtmlFieldset) getFormElement();
-//        fieldset.addData(visible);
-//        fieldset.setStyle("background-color: #D8D8D8");
-//        htmlHidden = new HtmlInputHidden(name, hidden);
-//        htmlHidden.setId(name);
-//
-//    }
-//
-//    public BShowField(String name, String label, String visible) {
-//        super(name + "_visible", label, new HtmlFieldset());
-//        fieldset = (HtmlFieldset) getFormElement();
-//        fieldset.addData(visible);
-//        fieldset.setStyle("background-color: #D8D8D8");
-//    }
-//
-//    public BShowField(String label, String visible) {
-//        this("", label, visible);
-//        fieldset.addData(visible);
-//    }
-//
-//    public BShowField setValues(String hidden, String visible) {
-//        fieldset.addData(visible);
-//        if (htmlHidden == null) {
-//            htmlHidden = new HtmlInputHidden(StringUtils.replace(getFormElement().getName(), "_visible", ""), hidden);
-//        }
-//        htmlHidden.setValue(hidden);
-//        return this;
-//    }
-//
-//    @Override
-//    public HtmlFormElement required() {
-//        super.required();
-//        htmlHidden.required();
-//        return this;
-//    }
-//
-//    @Override
-//    protected void preHtmlRender() {
-//        if (htmlHidden != null) {
-//            getDiv().add(htmlHidden);
-//        }
-//        super.preHtmlRender();
-//    }
-//
-//}
+package net.comtor.ocelot.bootstrap.forms;
+
+import net.comtor.ocelot.bootstrap.decorators.BootstrapFormElement;
+import net.comtor.ocelot.html.forms.inputs.HtmlInputHidden;
+import net.comtor.ocelot.html.forms.inputs.HtmlInputText;
+import org.apache.commons.lang3.StringUtils;
+
+public class BShowField extends BootstrapFormElement<HtmlInputText> {
+
+    private HtmlInputText showInput;
+    private HtmlInputHidden hiddenInput;
+
+    public BShowField(String label, String showValue, String name, String hiddenValue) {
+        super(label, new HtmlInputText());
+
+        showInput = getInput();
+        showInput.setValue(showValue);
+        showInput.addAttribute("read-only", "read-only");
+        showInput.setStyle("background-color: #D8D8D8");
+
+        if (name != null) {
+            hiddenInput = new HtmlInputHidden(name, hiddenValue);
+            hiddenInput.setId(name);
+        }
+    }
+
+    public BShowField(String label, String showValue, String name) {
+        this(label, showValue, name, "");
+    }
+
+    public BShowField(String label, String showValue) {
+        this(label, showValue, null);
+    }
+
+    public BShowField setValues(String showValue, String hiddenValue) {
+        showInput.setValue(showValue);
+
+        if (hiddenInput == null) {
+            String name = StringUtils.replace(getInput().getName(), "_visible", "");
+            hiddenInput = new HtmlInputHidden(name, hiddenValue);
+        }
+
+        hiddenInput.setValue(hiddenValue);
+
+        return this;
+    }
+
+    @Override
+    protected void preHtmlRender() {
+        if (hiddenInput != null) {
+            add(hiddenInput);
+        }
+
+        super.preHtmlRender();
+    }
+
+}
